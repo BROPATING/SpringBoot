8595d0a9839d6cfbe21ae430bfd9ffa5b1047d0a
BROPATING <sahilsalunke2004@gmail.com>
2026-07-09 07:25:32 +0530

    Code_Edit_5 Added SpringWebDataBase

---
+++ SpringWebDataBase/src/main/java/com/BROPATING/SpringWebDataBase/SpringWebDataBaseApplication.java
@@ -0,0 +1,13 @@
+package com.BROPATING.SpringWebDataBase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebDataBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebDataBaseApplication.class, args);
+	}
+
+}
---
+++ SpringWebDataBase/src/test/java/com/BROPATING/SpringWebDataBase/SpringWebDataBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BROPATING.SpringWebDataBase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebDataBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
